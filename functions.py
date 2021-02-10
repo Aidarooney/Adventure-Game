fe4b937fed9f8c00a2bee751a496b5626f481bc6
Aidarooney <ajmcwall@gmail.com>
2021-02-10 22:55:07 +0000

    added Hex challenge for left door, added walking function

--- functions.py
+++ functions.py
@@ -1,1 +1,14 @@
+import time
 
+def walking():
+    print("You start walking, you will arrive in:\n")
+    print("5\n")
+    time.sleep(1)
+    print("4\n")
+    time.sleep(1)
+    print("3\n")
+    time.sleep(1)
+    print("2\n")
+    time.sleep(1)
+    print("1\n")
+    time.sleep(1)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from time import *
+from functions import walking
+import time
 answer = input("Would you like to play? (yes/no)\n")
 enter = "Press Enter to continue..."
+HEXcode = "open"
+HEXinput = ""
+DoorOpen = False 
 
 if answer.lower().strip() == "yes":
     answer = input("You reach a cross roads, would you like to go left or right?\n").lower().strip()
@@ -14,12 +18,22 @@ if answer.lower().strip() == "yes":
 
         else:
             print("Good choice you made it away safely\n")
-            time.sleep(3)
+            walking()
             print("You come accross a tree with what appears to be a building on top of it and a keypad on a knot, there appears to be a note above it\n")
+            time.sleep(3)
             answer = input("Do you read the note or keep moving? (read/move)\n")
 
             if answer == "read":
-                print("The note reads, 'The owner of this base is a man for CTF, open the door and seek Consolation HEX: 6f 70 65 6e'")
+                print("The note reads, 'The owner of this base is a man for CTF, open the door and seek Consolation HEX: 6f 70 65 6e'\n")
+
+                while DoorOpen == False:
+                    HEXinput = input("Please enter code to unlock the secrets within\n")
+                    if HEXinput == HEXcode:
+                        DoorOpen = True
+                print("The door slides open revealling a ladder that you start to climb\n")
+                time.sleep(3)
+                
+
 
     elif answer == "right":
         answer = input("You find a building, would you like to go inside? (yes/no)\n")
@@ -28,7 +42,8 @@ if answer.lower().strip() == "yes":
             answer = input("You enter a room with a tool chest and 2 doors, one leads to the kitchen and the other leads to the garage (chest/kitchen/garage)\n")
         
         else:
-            print("You keep walking through the forest")
+            print("You keep walking through the forest\n")
+            walking()
             answer = input("You are starving and come across some berries, they look edible (eat/leave)\n")
 
             if answer == "eat":
@@ -36,7 +51,7 @@ if answer.lower().strip() == "yes":
                 input(enter)
 
             else:
-                print("You walk away from the bush hoping for a more reliable food source")
+                print("You walk away from the bush hoping for a more reliable food source\n")
                 
 
     else:
